7b100f0461b96bbc79d54c2a25d1e0803116ff4c
NikolaiKruchkov <47074017+NikolaiKruchkov@users.noreply.github.com>
2022-02-22 17:50:36 +0300

    Add files via upload

---
+++ ExportImage/Main.cs
@@ -0,0 +1,42 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExportImage
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Main : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            Document doc = commandData.Application.ActiveUIDocument.Document;
+            View view = doc.ActiveView;
+            string dEsktoppath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), view.Name);
+
+
+            using (var ts = new Transaction(doc, "export Image"))
+            {
+                ts.Start();
+                var imageOption = new ImageExportOptions
+                {
+                    ZoomType = ZoomFitType.FitToPage,
+                    FilePath = dEsktoppath,
+                    FitDirection = FitDirectionType.Horizontal,
+                    HLRandWFViewsFileType = ImageFileType.JPEGMedium,
+                    ExportRange = ExportRange.CurrentView,
+                    ViewName = view.Name
+                };              
+                    doc.ExportImage(imageOption);                
+                ts.Commit();
+            }
+
+            return Result.Succeeded;
+        }
+    }
+}
